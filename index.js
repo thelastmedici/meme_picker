aeebdb845e4c72847850d0f1de90fea46c86de59
user.name: "thelastmedici <user.email: "christianopeyemijoshua@gmail.com>
2023-10-07 17:12:27 +0100

    added functionality

--- index.js
+++ index.js
@@ -170,11 +170,13 @@ const catsData = [
 ]
 
 function getEmotionsArray(cats){
-    const emotionsArray = []
-    for (let i = 0; i < cats.length; i++){
-        for (let j=0; j < cats[i].emotionTags.length; j++){
-                emotionsArray.push(cats[i].emotionTags[j])
+    const emotionArray = []
+    for (let cat of cats){
+        for(let emotion of cats.emotionTags){
+            emotionArray.push(emotion)
         }
     }
-console.log(emotionsArray)
+    return emotionArray
 }
+
+getEmotionsArray(catsData)
